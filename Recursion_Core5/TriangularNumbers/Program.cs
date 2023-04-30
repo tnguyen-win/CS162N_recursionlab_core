64cd1cbdc92cc0fd9a99dbd74d2e09fd830a3279
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-04-30 13:29:12 -0700

    Lab 3 - Draft - TriangularNumbers

--- Recursion_Core5/TriangularNumbers/Program.cs
+++ Recursion_Core5/TriangularNumbers/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace TriangularNumbers
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+namespace TriangularNumbers {
+	class Program {
+		static void Main() {
+			for (int n = 1; n <= 10; n++) Console.WriteLine($"[{n}] - {recursion(n)}");
+		}
+
+		static int recursion(int n) {
+			if (n == 1) return 1;
+			else return n + recursion(n - 1);
+		}
+	}
 }
