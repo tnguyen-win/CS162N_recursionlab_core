2d0c4207c7c3ae40380ceb66f9c703e80dd7ec65
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-04-30 13:29:20 -0700

    Lab 3 - Draft - FibonacciNumbers

--- Recursion_Core5/FibonacciNumbers/Program.cs
+++ Recursion_Core5/FibonacciNumbers/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace FibonacciNumbers
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+namespace FibonacciNumbers {
+	class Program {
+		static void Main() {
+			for (int n = 0; n < 10; n++) Console.WriteLine($"[{n}] - {recursion(n)}");
+		}
+
+		static int recursion(int n) {
+			if (n > 1) return recursion(n - 1) + recursion(n - 2);
+			else return n;
+		}
+	}
 }
